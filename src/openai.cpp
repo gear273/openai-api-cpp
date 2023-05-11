2ad453f7dbececf19a014770e3e81d3b3736012a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-10 21:47:39 -0400

    refactor: Add core api implementation
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- src/openai.cpp
+++ src/openai.cpp
@@ -1,6 +1,37 @@
+#include <iostream>
 #include "openai.h"
 
-int main(int argc, char **argv)
+OpenAI::OpenAI(const std::string &api_key)
+	: api_key(api_key)
 {
-	return 0;
+}
+
+cpr::Header OpenAI::get_headers()
+{
+	cpr::Header headers = {
+		{"Authorization", "Bearer " + api_key},
+		{"Content-Type", "application/json"}};
+
+	return headers;
+}
+
+cpr::Response OpenAI::models()
+{
+	return cpr::Get(cpr::Url{OpenAI_API::MODELS}, get_headers());
+}
+
+cpr::Response OpenAI::completions(const std::string &prompt)
+{
+	cpr::Payload payload = {
+		{"prompt", prompt}};
+
+	return cpr::Post(cpr::Url{OpenAI_API::COMPLETIONS}, get_headers(), payload);
+}
+
+cpr::Response OpenAI::chat_completions(const std::vector<std::string> &messages)
+{
+	cpr::Payload payload = {
+		{"messages", messages}};
+
+	return cpr::Post(cpr::Url{OpenAI_API::CHAT_COMPLETIONS}, get_headers(), payload);
 }
