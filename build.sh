2730c852be4e1e152ba421046b48419cedd48930
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-14 02:34:48 -0400

    refactor: Fix and clean up scripts
    - Extract build code to a function in build.sh
    - Remove building chat from run.sh

--- build.sh
+++ build.sh
@@ -1,52 +1,45 @@
 #!/usr/bin/env bash
 
+function build() {
+  local module_path="${1}"
+  cd "${module_path}" || (echo "Error: Missing '$module_path'" && exit 1)
+  mkdir -p build
+  cd build || (echo "Error: Building '$module_path' failed" && exit 1)
+  cmake .. || (echo "Error: Failed to locate CMakeLists.txt for '$module_path'" && exit 1)
+  make || (echo "Error: Building '$module_path' failed" && exit 1)
+  cd "${2}" || (echo "Error: Failed to exit '$module_path'" && exit 1)
+}
+
 # Prompt user to choose module to build
 echo "Which module would you like to build?"
 echo "1. cpr (submodule)"
 echo "2. cpp-tiktoken (submodule)"
 echo "3. openai-api-cpp (module)"
 
-read -p "Enter your choice (1, 2, or 3): " choice
+read -rp "Enter your choice (1, 2, or 3): " choice
 
 case $choice in
   1)
     # Build cpr submodule
-    cd submodules/cpr || (echo "Error: Missing 'submodules/cpr'" && exit 1)
-    mkdir -p build
-    cd build || (echo "Error: Building 'submodules/cpr' failed" && exit 1)
-    cmake ..
-    make
-
-    echo "cpr submodule build completed."
+    build "submodules/cpr" ../../../
+    echo "'submodules/cpr' build completed."
     ;;
 
   2)
     # Build cpp-tiktoken submodule
-    cd submodules/cpp-tiktoken || (echo "Error: Missing 'submodules/cpp-tiktoken'" && exit 1)
-    mkdir -p build
-    cd build || (echo "Error: Building 'submodules/cpp-tiktoken' failed" && exit 1)
-    cmake ..
-    make
-
-    echo "cpp-tiktoken submodule build completed."
+    build "submodules/cpp-tiktoken" ../../../
+    echo "'submodules/cpp-tiktoken' build completed."
     ;;
 
   3)
     # Build openai-api-cpp module
-    mkdir -p build
-    cd build || (echo "Error: Building 'submodules/pybind11' failed" && exit 1)
-    cmake ..
-    make
-
-    echo "openai-api-cpp build completed."
+    build "." ../
+    cp -r ./submodules/cpp-tiktoken/build/tokenizers ./build/tokenizers
+    echo "'openai-api-cpp' build completed."
     ;;
-
 
   *)
     echo "Invalid choice. Exiting."
     exit 1
     ;;
 esac
-
-# Return to the repository root directory
-cd ../../../
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-make chat || exit
 source .env
 export OPENAI_API_KEY
 ./build/chat
