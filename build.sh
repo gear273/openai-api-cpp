8e5230833a227901947b563b82083e0095d43869
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-13 23:36:41 -0400

    refactor: Add options for building submodules
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,18 +1,53 @@
 #!/usr/bin/env bash
 
-# build cpr
-cd submodules/cpr || (echo "Error: Missing 'submodules/cpr'" && exit)
-mkdir -p build
-cd build || (echo "Error: Building 'submodules/cpr' failed" && exit)
-cmake ..
-make
+# Prompt user to choose submodule to build
+echo "Which submodule would you like to build?"
+echo "1. cpr"
+echo "2. cpp-tiktoken"
+echo "3. pybind11"
 
-# return to repository root directory
+read -p "Enter your choice (1, 2, or 3): " choice
+
+case $choice in
+  1)
+    # Build cpr submodule
+    cd submodules/cpr || (echo "Error: Missing 'submodules/cpr'" && exit 1)
+    mkdir -p build
+    cd build || (echo "Error: Building 'submodules/cpr' failed" && exit 1)
+    cmake ..
+    make
+
+    echo "cpr submodule build completed."
+    ;;
+
+  2)
+    # Build cpp-tiktoken submodule
+    cd submodules/cpp-tiktoken || (echo "Error: Missing 'submodules/cpp-tiktoken'" && exit 1)
+    mkdir -p build
+    cd build || (echo "Error: Building 'submodules/cpp-tiktoken' failed" && exit 1)
+    cmake ..
+    make
+
+    echo "cpp-tiktoken submodule build completed."
+    ;;
+
+  3)
+    # Build pybind11 submodule
+    cd submodules/pybind11 || (echo "Error: Missing 'submodules/pybind11'" && exit 1)
+    mkdir -p build
+    cd build || (echo "Error: Building 'submodules/pybind11' failed" && exit 1)
+    cmake ..
+    make check -j 4
+
+    echo "pybind11 submodule build completed."
+    ;;
+
+
+  *)
+    echo "Invalid choice. Exiting."
+    exit 1
+    ;;
+esac
+
+# Return to the repository root directory
 cd ../../../
-
-# build pybind11
-cd submodules/pybind11 || (echo "Error: Missing 'submodules/pybind11'" && exit)
-mkdir -p build
-cd build || (echo "Error: Building 'submodules/pybind11' failed" && exit)
-cmake ..
-make check -j 4
